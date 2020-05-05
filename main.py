c98b826fb0e9d03ec1831797a078d5cf16b48a0a
Ricardo Arrobo <ricardoarrobo@gmail.com>
2020-05-05 18:46:05 -0500

    add: xlsx to Json parser

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import pandas
 
+# Data cleaning functions
+import data_cleaner
+# Json functions
+import json_maker
 
+# Reads xlsx File
 excel_raw_data = pandas.read_excel('./data/dataset.xlsx', sheet_name='Hoja1', header=1)
+# Get colum_names
 column_names = excel_raw_data.columns
+# Organization Fields: Areas, Departamento, Seccion
 organization_idx = column_names[2:5]
 values = excel_raw_data.values
 
-# print(organization_idx)
+# Create a set for each Organization field
+depts = set()
+areas = set()
+sections = set()
 
-# print("Area: ", values[1][2] + " Departamento: ", values[1][3] + " Seccion: ", values[1][4])
+for value in values:
+    temp = value[2:5]
+    for i in range(len(temp)):
+        if type(temp[i]) != float:
+            value = data_cleaner.normalize(temp[i])
+            if i == 0:
+                areas.add(value)
+            elif i == 1:
+                depts.add(value)
+            else:
+                sections.add(value)
 
-
-
-
-def getUniqueValues(list1):
-
-    for i in range(len(values)):
-        temp = values[i][2:5]
-        temp.to
-        print(type(temp))
-
-
-getUniqueValues(values)
+depts_data = json_maker.list_to_json(list(depts))
+json_maker.write_json(depts_data, './data/depts_data.json');
 
