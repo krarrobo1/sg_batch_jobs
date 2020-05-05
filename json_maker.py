1f003474df12a8eb43e409da523643f672fe08fe
Ricardo Arrobo <ricardoarrobo@gmail.com>
2020-05-05 18:45:17 -0500

    add: json maker module added

---
+++ json_maker.py
@@ -0,0 +1,15 @@
+import json
+import data_cleaner
+
+def list_to_json(my_list):
+    dictionaries = list()
+    for item in my_list:
+        id = data_cleaner.id_maker(item)
+        dictionaries.append({ id: item })
+
+    return dictionaries
+
+
+def write_json(data, directory):
+    with open(directory, 'w') as fp:
+        json.dump(data, fp)
