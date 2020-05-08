b49d7cf313880ff422f421f3bc700abb817e4229
Ricardo Arrobo <ricardoarrobo@gmail.com>
2020-05-07 19:32:31 -0500

    Refactorization

--- data_cleaner.py
+++ data_cleaner.py
@@ -4,9 +4,13 @@ def normalize(text):
     text = text.upper()
     normalized = unicodedata.normalize('NFKD', text).encode('ASCII', 'ignore')
     return normalized.decode('utf-8').strip()
+
+def remove_spaces(text):
+    return text.strip()
  
 # Removes connectors, and replace with '-'.
 def id_maker(raw):
+    raw = normalize(raw)
     raw = raw.lower()
     connectors = [' y ', ' o ', ' de ', ' la ', ' e ', ' u ', ' del ', ', ']
     for connector in connectors:
--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-import pandas
+import pandas as pd
+import data_cleaner as dc
 
-# Data cleaning functions
-import data_cleaner
-# Json functions
-import json_maker
+def join_data(arr): 
+    return ";".join(str(v) for v in arr)
 
-# Reads xlsx File
-excel_raw_data = pandas.read_excel('./data/dataset.xlsx', sheet_name='Hoja1', header=1)
-# Get colum_names
-column_names = excel_raw_data.columns
-# Organization Fields: Areas, Departamento, Seccion
-organization_idx = column_names[2:5]
-values = excel_raw_data.values
+def get_relations(values, start, end):
+    relation = set()
+    for value in values:
+        rows = value[start:end]
+        relation.add(join_data(rows))
+    return relation
 
-# Create a set for each Organization field
-depts = set()
-areas = set()
-sections = set()
+def get_labels_and_ids(values, idx):
+    labels_ids = set()
+    for value in values:
+        rows = value[idx]
+        label = str(rows).strip()
+        id = dc.id_maker(str(rows))
+        label_id = "label:"+label+","+"id:"+id
+        labels_ids.add(label_id)
 
-for value in values:
-    temp = value[2:5]
-    for i in range(len(temp)):
-        if type(temp[i]) != float:
-            value = data_cleaner.normalize(temp[i])
-            if i == 0:
-                areas.add(value)
-            elif i == 1:
-                depts.add(value)
-            else:
-                sections.add(value)
+    return labels_ids
 
-depts_data = json_maker.list_to_json(list(depts))
-json_maker.write_json(depts_data, './data/depts_data.json');
+def main():
+    df= pd.read_excel('./data/dataset.xlsx', sheet_name='Hoja1', header=1)
+    # # Relacion areas - departamentos
+    # area_dept_rel = get_relations(df.values, 2,4)
+    # # Relacion departamentos - secciones
+    # dept_sect_rel = get_relations(df.values, 3,5)
 
+    # print(area_dept_rel)
+    # print("")
+    # print(dept_sect_rel)
+    print('Areas \n')
+    print(get_labels_and_ids(df.values, 2))
+    print('Departments \n')
+    print(get_labels_and_ids(df.values, 3))
+    print('Sections \n')
+    print(get_labels_and_ids(df.values, 4))
+
+
+if __name__ == '__main__':
+    main()
+    
